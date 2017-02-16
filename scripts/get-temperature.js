2cadc2a82718ae2f98d4aaf5ffde7b7405a8f64d
mkucharz <m@kucharz.net>
2017-02-16 11:22:38 +0100

    Initial commit

---
+++ scripts/get-temperature.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+console.log(`http://api.openweathermap.org/data/2.5/weather?q=${ARGS.city}&APPID=${CONFIG.APP_ID}`)
+
+axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${ARGS.city}&appid=${CONFIG.APP_ID}`)
+  .then((response) => response.data)
+  .then((weather) => {
+    const city = weather.name;
+    const temperatureKelvin = weather.main.temp;
+    const temperatureCelsius =  temperatureKelvin - 273.15;
+
+    setResponse(new HttpResponse(
+      200,
+      JSON.stringify({ temp: temperatureCelsius.toFixed(1)}),
+      META.metadata.response.mimetype
+    ));
+  })
+  .catch((error) => {
+    setResponse(new HttpResponse(
+      400,
+      JSON.stringify({ message: error.response.data.message }),
+      META.metadata.response.mimetype
+    ));
+  });
